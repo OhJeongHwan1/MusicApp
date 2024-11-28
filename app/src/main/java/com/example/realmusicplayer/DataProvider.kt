ab60a184f7c1fb9ac9a09bc24e36bd4efc0cda0c
OhJeongHwan1 <wjdghks0316@naver.com>
2024-11-28 14:20:30 +0900

    feat: 난 메인 페이지(목록)

---
+++ app/src/main/java/com/example/realmusicplayer/DataProvider.kt
@@ -0,0 +1,38 @@
+package com.example.realmusicplayer
+
+object DataProvider {
+    fun getSongs(): List<Song> {
+        return listOf(
+            Song(R.drawable.image1, "눈의 꽃", "박효신", "5:40", R.raw.song1),
+            Song(R.drawable.image2, "좋은 날", "아이유", "3:55", R.raw.song2),
+            Song(R.drawable.image3, "비와 당신의 이야기", "부활", "6:01", R.raw.song3),
+            Song(R.drawable.image4, "시간을 거슬러", "린", "3:31", R.raw.song4),
+            Song(R.drawable.image5, "너의 의미", "아이유 (Feat. 김창완)", "3:16", R.raw.song5),
+            Song(R.drawable.image6, "벚꽃 엔딩", "버스커 버스커", "4:22", R.raw.song6),
+            Song(R.drawable.image7, "그때 그 아인", "김필", "4:50", R.raw.song7),
+            Song(R.drawable.image8, "취중진담", "김동률", "5:21", R.raw.song8),
+            Song(R.drawable.image9, "서쪽 하늘", "이승철", "4:03", R.raw.song9),
+            Song(R.drawable.image10, "Everyday", "WINNER", "3:27", R.raw.song10),
+            Song(R.drawable.image11, "봄여름가을겨울", "빅뱅", "3:09", R.raw.song11),
+            Song(R.drawable.image12, "봄날", "방탄소년단", "4:35", R.raw.song12),
+            Song(R.drawable.image13, "사랑을 했다", "iKON", "3:20", R.raw.song13),
+            Song(R.drawable.image14, "너를 만나", "폴킴", "4:41", R.raw.song14),
+            Song(R.drawable.image15, "지나오다", "닐로", "4:20", R.raw.song15),
+            Song(R.drawable.image16, "너였다면", "정승환", "4:42", R.raw.song16),
+            Song(R.drawable.image17, "노래방에서", "장범준", "3:59", R.raw.song17),
+            Song(R.drawable.image18, "야생화", "박효신", "5:12", R.raw.song18),
+            Song(R.drawable.image19, "좋니", "윤종신", "5:28", R.raw.song19),
+            Song(R.drawable.image20, "Celebrity", "아이유", "3:16", R.raw.song20),
+            Song(R.drawable.image21, "아로하", "조정석", "4:05", R.raw.song21),
+            Song(R.drawable.image22, "나만, 봄", "볼빨간사춘기", "3:37", R.raw.song22),
+            Song(R.drawable.image23, "모든 날, 모든 순간", "폴킴", "3:30", R.raw.song23),
+            Song(R.drawable.image24, "우주를 줄게", "볼빨간사춘기", "3:34", R.raw.song24),
+            Song(R.drawable.image25, "팔레트", "아이유 (Feat. G-Dragon)", "3:34", R.raw.song25),
+            Song(R.drawable.image26, "Supernova", "aespa", "2:58", R.raw.song26),
+            Song(R.drawable.image27, "Ditto", "NewJeans", "3:06", R.raw.song27),
+            Song(R.drawable.image28, "드라마", "아이유", "2:08", R.raw.song28),
+            Song(R.drawable.image29, "Lovely", "악동뮤지션", "3:00", R.raw.song29),
+            Song(R.drawable.image30, "OMG", "NewJeans", "3:32", R.raw.song30)
+        )
+    }
+}
